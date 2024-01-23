1437b8356c85803cdb40cef2783b6a5ac2af8a16
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-01-23 21:52:51 +0600

    Add files via upload

---
+++ aelephant.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a ;
+cin >> a;
+
+int sum = 0;
+sum = a/5;
+int rum = sum+1;
+if(a%5==0){
+    cout <<sum;
+
+}
+else
+    cout<<rum;
+}
