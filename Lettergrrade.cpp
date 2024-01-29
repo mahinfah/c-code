509d5b42ae7fda2d92e05f48da981030f3dec1ca
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-01-29 23:29:43 +0600

    Add files via upload

---
+++ Lettergrrade.cpp
@@ -0,0 +1,77 @@
+if (num>80){
+    cout << "A+";
+}
+#include<iostream>
+#include<conio.h>
+
+using namespace std ;
+
+ int main ()
+
+ {
+int num ;
+cout << "Type your marks :";
+cin >> num ;
+
+if(num>100){
+    cout<< " invalid number";
+}
+if (num<=0){
+    cout << "invalid number";
+}
+else if (num>80){
+    cout << "A+";
+}
+
+
+
+else if (num>70){
+    cout << "A";
+}
+
+
+
+
+else if (num>60){
+    cout << "A-";
+}
+
+
+else if (num>50){
+    cout << "B";
+}
+
+
+
+else if (num>40){
+    cout << "C";
+}
+
+else if (num>=33){
+    cout << "passed" ;
+}
+else {
+    cout << "failed";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    getch () ;
+
+ }
+
