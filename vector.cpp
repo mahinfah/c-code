e0cc01a8f62765b3408b79c690000ea94a611e60
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-01 11:14:12 -0800

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+vector<int> v ;
+v.push_back(2);
+v.push_back(3);
+v.push_back(5);
+v.push_back(2);
+v.push_back(2);
+v.push_back(2);
+//cout<<v[2]<<endl;
+for (int i = 0  ; i < v.size();i++){
+    cout<<v[i]<<endl;
+}
+
+
+
+
+
+
+
+
+}
