0ce1f0f2a8cb1c9fc721363afb667a985e1ea8f1
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-01 11:28:33 -0800

    Add files via upload

---
+++ vectoruserinput.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n;
+vector<int> v;
+cout <<"enter the vector length"<<endl;
+cin>>n;
+for(int i = 0 ; i<=n;i++){
+int a;
+cin>>a;
+v.push_back(a);
+
+}
+
+for(int i = 0;i<v.size();i++)
+{
+
+    cout <<v[i]<<endl;
+}
+
+
+
+
+
+}
