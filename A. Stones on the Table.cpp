d7fd1dff1c5e3b898d434e38b159aa216001f84d
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-16 21:02:02 +0600

    Add files via upload

---
+++ A. Stones on the Table.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+#include<string>
+using namespace std ;
+int main(){
+    int v ;
+    cin>> v;
+string a ;
+cin >> a;
+int count = 0;
+for(int i = 0 ; i<v;i++){
+    if(a[i]==a[i+1]){
+    count++;}
+
+
+}
+
+
+
+cout<<count ;
+
+
+
+
+
+
+
+return 0 ;
+}
