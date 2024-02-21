27a8bf01d9cb6e780d7155e90e1b0ceb2c570335
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-21 14:10:33 +0600

    Add files via upload

---
+++ A. Odd One Out.cpp
@@ -0,0 +1,43 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int n ;
+cin>>n;
+int a[n*3];
+for(int i = 0 ; i<n*3 ; i++){
+
+        cin>>a[i];
+
+    }
+
+
+    for(int i = 0 ; i<n ; i++){
+
+if(a[0+i*3]==a[1+i*3]){
+    cout<<a[2+i*3]<<endl;
+
+
+}
+else if (a[2+i*3]==a[1+i*3]){
+    cout<<a[0+i*3]<<endl;;
+  //  break;
+}
+else if (a[0+i*3]==a[2+i*3]){
+    cout<<a[1+i*3]<<endl;}
+//break;
+}
+
+    }
+
+
+
+
+
+
+
+
+
+
