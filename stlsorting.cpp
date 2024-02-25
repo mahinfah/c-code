7531e2e65c72c0993396b426876fc812f259458b
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-25 23:56:22 +0600

    Add files via upload

---
+++ stlsorting.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+vector<int>v={5,3,23,4,3,4,2};
+sort(v.begin(), v.begin()+4 );
+
+for(auto u : v ) cout<< u <<" ";
+cout<<endl;
+
+
+
+
+}
