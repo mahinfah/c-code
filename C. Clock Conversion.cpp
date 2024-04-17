d8a49d45a3ac83e207ad2708da2384fc0d973052
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-04-17 22:06:39 +0600

    Add files via upload

---
+++ C. Clock Conversion.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b ;
+  //  int a ;
+    cin>>a;
+
+
+    for(int i = 1 ; i<=a;i++)
+    {
+        string m;
+        cin>>m;
+
+        if(m[0]=='0'&& m[1]=='1')
+        {
+            cout<<m<<" AM"<<endl;
+        }
+        else if(m[0]=='0'&& m[1]=='2')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='3')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='4')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='5')
+         {
+               cout<<m<<" AM"<<endl;
+         }
+         else if(m[0]=='0'&& m[1]=='6')
+            {
+ cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='7')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='8')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+
+         else if(m[0]=='0'&& m[1]=='9')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='0')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='1')
+            {
+            cout<<m<<" AM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='2')
+            {
+            cout<<"12"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='0'&& m[1]=='0')
+            {
+            cout<<"12"<<m[2]<<m[3]<<m[4]<<" AM"<<endl;
+        }
+         //else if(m[0]=='1'&& m[1]=='1')
+           // {
+            //cout<<"01"<<" PM"<<endl;
+
+            //}
+         //else if(m[0]=='1'&& m[1]=='2')
+           // {
+            ///cout<<"01"<<" PM"<<endl;
+        //}
+        else if(m[0]=='1'&& m[1]=='3')
+            {
+            cout<<"01"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='4')
+            {
+            cout<<"02"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='5')
+            {
+            cout<<"03"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='6')
+            {
+            cout<<"04"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='7')
+            {
+            cout<<"05"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='8')
+            {
+            cout<<"06"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='1'&& m[1]=='9')
+            {
+            cout<<"07"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='2'&& m[1]=='0')
+            {
+            cout<<"08"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='2'&& m[1]=='1')
+            {
+            cout<<"09"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+
+         else if(m[0]=='2'&& m[1]=='2')
+            {
+            cout<<"10"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='2'&& m[1]=='3')
+            {
+            cout<<"11"<<m[2]<<m[3]<<m[4]<<" PM"<<endl;
+        }
+         else if(m[0]=='2'&& m[1]=='4')
+            {
+            cout<<"12"<<m[2]<<m[3]<<m[4]<<" AM"<<endl;
+        }
+
+
+
+}}
+
+
