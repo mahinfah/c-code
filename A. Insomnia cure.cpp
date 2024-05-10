bf6b9d95b2e4663b0d8706214ee06e98f18e87b3
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-05-10 14:45:24 +0600

    Add files via upload

---
+++ A. Insomnia cure.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k,l,m,n,d;
+
+    cin>>k;
+    cin>>l;
+    cin>>m;
+    cin>>n;
+    cin>>d;
+     int count=0;
+     for(int i  = 1 ;i<=d;i++)
+     {
+       if(i%k==0)
+       {
+           count++;
+       }
+
+         else if (i%l==0)
+         {
+             count++;
+         }
+         else if (i%m==0)
+         {
+             count++;
+         }
+         else if (i%n==0)
+         {
+             count++;
+         }
+         else if (i%l==0)
+         {
+             count++;
+         }
+
+     }
+
+     cout<<count;
+
+}
+
+
+
+
+
+
