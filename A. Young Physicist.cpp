aca2c3550c43ceb0d8a3264f447747beea413a65
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-05-28 20:19:37 +0600

    Update A. Young Physicist.cpp

--- A. Young Physicist.cpp
+++ A. Young Physicist.cpp
@@ -1,49 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main() 
+int main()
 {
-   //
-  //  cout << "Try programiz.pro";
-int n; 
-cin>> n;
-int m = n*3;
-int a [m];
-int sum1= 0;
-int sum2= 0;
-int sum3= 0;
- 
- 
-for(int i = 0 ; i < m ; i++ )
+
+int n ;
+cin>>n;
+int a[100][100];
+int sum1=0,sum2=0,sum3=0;
+for(int i = 0 ;i <n ;i++)
+{
+  for(int j = 0 ; j<3;j++)
   {
+      cin>>a[i][j];
+      
+  }
+    sum1=sum1+a[i][0];
+    sum2=sum2+a[i][1];
+    sum3 =sum3+ a[i][2];
+
+  }
+
+    // cout<<endl;
     
-      sum1 = sum1 +a[i*n];
-      
-  }
-  cout<<sum1<<endl;
-for(int i = 0 ; i < m ; i++ )
-  {
-    int p = i*n;
-      sum2 = sum2 +a[p+1];
-      
-  }
-  cout<<sum2<<endl;
- 
- for(int i = 0 ; i < m ; i++ )
-  {
-    int q = i*n;
-      sum3 = sum3 +a[q+2];
-      
-  }
-  cout<<sum3<<endl;
-  if(sum1==0||sum2==0||sum3==0)
+//cout<<sum1<<endl;
+
+  //cout<<sum2<<endl;
+//
+ // cout<<sum3<<endl;
+  if(sum3==0&&sum2==0&&sum1==0)
   {
       cout<<"YES"<<endl;
+
   }
- 
- else
- {
-     cout<<"NO"<<endl;
- }
-    return 0;
+  else
+  {
+      cout<<"NO"<<endl;
+  }
 }
