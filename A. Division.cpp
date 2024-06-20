7f25ad5941b41b2638ea7063852accb9bcf19fd7
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-06-20 21:00:12 +0600

    Add files via upload

---
+++ A. Division.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int check(int n)
+{
+    for(int i = 1 ;i<=n;i++)
+    {
+       int a;
+       cin>>a;
+
+       if(a>=1900)
+       {
+           cout<<"Division 1"<<endl;
+       }
+       else if(a>=1600&&a<=1899)
+       {
+          cout<<"Division 2"<<endl;
+
+       }
+
+       else if(a>=1400&&a<=1599)
+       {
+          cout<<"Division 3"<<endl;
+
+       }
+
+       else if(a<=1399)
+       {
+          cout<<"Division 4"<<endl;
+
+       }
+    }
+}
+
+
+int main()
+{
+
+int n;
+cin>>n;
+
+    check(n);
+}
