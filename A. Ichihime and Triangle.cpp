4afc402504b31bf6d1e371769ebdaab85779c0c9
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2024-07-16 23:38:23 +0600

    A. Ichihime and Triangle.cpp

---
+++ A. Ichihime and Triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;cin>>n;
+      int aa =0,bb=0,cc=0;
+    for(int i = 1;i<=n;i++)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        aa=b;
+        bb=c;
+        cc=c;
+        cout<<aa<<" "<<bb<<" "<<cc<<endl;
+       }
+
+
+  
+}
