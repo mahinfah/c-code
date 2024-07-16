00d0c80f99bad22217d61c01719024f2868be8f8
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2024-07-16 23:39:24 +0600

    Create A. Again Twenty Five!.cpp

---
+++ A. Again Twenty Five!.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   long long int n;
+   long long int  nn=1;
+   cin>>n;
+   for(int i = 1;i<=n;i++)
+   {
+       nn=nn*5;
+   }
+    cout<<25;
+    return 0;
+}
