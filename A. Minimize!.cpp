0de82a57c617f0f0a5a15c6493be5f1ccb4b0061
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2024-09-18 22:44:22 -0800

    Create A. Minimize!.cpp

---
+++ A. Minimize!.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i =1 ;i<=n;i++)
+    {
+        int a;
+        int b;
+        cin>>a;
+        cin>>b;
+
+          cout<<b-a<<endl;
+    }
+
+}
